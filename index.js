346dcbfd6b52908462eb2a33f966249971725357
ByeongSu Do <bsdo@naver.com>
2016-04-17 21:17:47 +0900

    publish Models

--- index.js
+++ index.js
@@ -11,17 +11,4 @@ const knex = Knex(connection);
 // Give the connection to objection.
 Model.knex(knex);
 
-var M = require('./Models/index');
-
-M
-  .tc_users
-  .query()
-  .insert({email: 'Jenndif1er1', nick: 'hel2lo2', uid: 'hㄴsdsd33fello'})
-  .then(function (jennifer) {
-    console.log(jennifer instanceof M.tc_users); // --> true
-    console.log(jennifer.email); // --> 'Jennifer'
-    console.log(jennifer.fullName()); // --> 'Jennifer Lawrence'
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
+module.Models = require('./Models/index');
